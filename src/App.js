29a1a32548058e71b99640a2fdd7195dd8365163
Habib Yahya <nmh.habib.y@gmail.com>
2022-03-06 23:01:39 +0700

    [Add] Project Migrate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Profile from './components/Profile'
+import './App.css'
+import ProjectShowcase from './components/ProjectShowcase';
+
+function App() {
+  return (
+    <div className="App">
+        <Profile/>
+        <ProjectShowcase/>
+    </div>
+  );
+}
+
+export default App;
